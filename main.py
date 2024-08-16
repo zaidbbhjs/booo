35af5a621fcd450268c1890d73a28287b0ccd487
zaidbbhjs <151960814+zaidbbhjs@users.noreply.github.com>
2024-08-17 00:57:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,21 @@
 from fastapi import Response
 import requests
 import os
-import random
+import random,uuid
 import uvicorn
 from fastapi import Request
-import json,faker
 from user_agent import generate_user_agent
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
-import secrets,uuid
+import secrets
 cok = secrets.token_hex(8) * 2
 app = FastAPI()
 a=int(0)
 from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
-
-from requests import post as pp
-from user_agent import generate_user_agent as gg
-from random import choice as cc
-from random import randrange as rr
-from faker import Faker
-yy='azertyuiopmlkjhgfdsqwxcvbn'
-@app.get('/api2/{gmail}')
-async def ga(gmail: str, request: Request):
-	from requests import post
-	email=gmail
-	mj=0
-	url='https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/'
-	h={
- 'X-Pigeon-Session-Id':'2b712457-ffad-4dba-9241-29ea2f472ac5',
- 'X-Pigeon-Rawclienttime':'1707104597.347',
- 'X-IG-Connection-Speed':'-1kbps',
- 'X-IG-Bandwidth-Speed-KBPS':'-1.000',
- 'X-IG-Bandwidth-TotalBytes-B':'0',
- 'X-IG-Bandwidth-TotalTime-MS':'0',
- 'X-IG-VP9-Capable':'false',
- 'X-Bloks-Version-Id':'009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
- 'X-IG-Connection-Type':'WIFI',
- 'X-IG-Capabilities':'3brTvw==',
- 'X-IG-App-ID':'567067343352427',
- 'User-Agent':'Instagram 100.0.0.17.129 Android (30/11; 320dpi; 720x1448; realme; RMX3231; RMX3231; RMX3231; ar_IQ; 161478664)',
- 'Accept-Language':'ar-IQ, en-US',
- 'Cookie':'mid=Zbu4xQABAAE0k2Ok6rVxXpTD8PFQ; csrftoken=dG4dEIkWvAWpIj1B2M2mutWtdO1LiPCK',
- 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
- 'Accept-Encoding':'gzip, deflate',
- 'Host':'i.instagram.com',
- 'X-FB-HTTP-Engine':'Liger',
- 'Connection':'keep-alive',
- 'Content-Length':'364',
- }
-	da={
- 'signed_body':'ef02f559b04e8d7cbe15fb8cf18e2b48fb686dafd056b7c9298c08f3e2007d43.{"_csrftoken":"dG4dEIkWvAWpIj1B2M2mutWtdO1LiPCK","adid":"5e7df201-a1ff-45ec-8107-31b10944e25c","guid":"b0382b46-1663-43a7-ba90-3949c43fd808","device_id":"android-71a5d65f74b8fcbc","query":"'f'{email}''"}',
- 
- 'ig_sig_key_version':'4',
- }
-	s=post(url,headers=h,data=da).json()
- #print(s)
-	if s["status"]=="ok":
-		
-		return         JSONResponse(content={"status":"Good"})
-	else:
-		return         JSONResponse(content=s)
-#uvicorn.run(app,host='0.0.0.0',port=8080)
 @app.get('/tik/{email}')
-async def tik():
+async def tik(email):
 		sis = str(uuid.uuid4()).replace('-', '')
 		url =f'https://api22-normal-c-alisg.tiktokv.com/passport/email/bind_without_verify/?passport-sdk-version=19&iid=7372841843832473349&device_id=7194351170030650885&ac=WIFI&channel=googleplay&aid=1233&app_name=musical_ly&version_code=310503&version_name=31.5.3&device_platform=android&os=android&ab_version=31.5.3&ssmix=a&device_type=Infinix+X6816&device_brand=Infinix&language=ar&os_api=30&os_version=11&openudid=3293d1a6e9361cb7&manifest_version_code=2023105030&resolution=720*1568&dpi=303&update_version_code=2023105030&_rticket=1722418820230&is_pad=0&current_region=IQ&app_type=normal&sys_region=IQ&mcc_mnc=41805&timezone_name=Asia%2FBaghdad&carrier_region_v2=418&residence=IQ&app_language=ar&carrier_region=IQ&ac2=wifi5g&uoo=0&op_region=IQ&timezone_offset=10800&build_number=31.5.3&host_abi=arm64-v8a&locale=ar&region=IQ&content_language=ar%2C&ts=1722418819&cdid=556d8162-2721-4760-a509-a92b3cf27738&support_webview=1&cronet_version=2fdb62f9_2023-09-06&ttnet_version=4.2.152.11-tiktok&use_store_region_cookie=1'
 		headers = {
